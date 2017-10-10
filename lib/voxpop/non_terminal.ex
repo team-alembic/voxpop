27beac3323b1fea2383695729675bf0ac3fb0b65
Josh Price <josh@alembic.com.au>
2017-10-10 16:25:13 +1100

    Show error inline perhaps?

--- lib/voxpop/non_terminal.ex
+++ lib/voxpop/non_terminal.ex
@@ -5,6 +5,8 @@ end
 
 defimpl Voxpop.Production, for: Voxpop.NonTerminal do
   def evaluate(non_terminal, registry) do
-    registry.rules |> Map.get(non_terminal.rule) |> Voxpop.Production.evaluate(registry)
+    registry.rules
+    |> Map.get(non_terminal.rule, %Voxpop.Terminal{atom: "<missing rule `#{non_terminal.rule}`>"})
+    |> Voxpop.Production.evaluate(registry)
   end
 end
